2ee7c12cc3e04bf84b50957ada7c7a72bd2437be
shivapbhusal <civabhusal@gmail.com>
2017-02-26 20:00:26 -0500

    yearly statistics added

---
+++ getYearlyData.php
@@ -0,0 +1,56 @@
+<?php
+ini_set('display_errors', 1); 
+    $username = "root"; 
+    $password = "";   
+    $host = "localhost";
+    $database="bigdata";
+    
+    $fromyear=1901;
+    $toyear=1940;  
+    
+    $server = mysql_connect($host, $username, $password);
+    $connection = mysql_select_db($database, $server);
+
+    $myquery = "SELECT * FROM `yearly_data` WHERE (year<=$toyear)&&(year>=$fromyear)";
+
+    $query = mysql_query($myquery);
+    $table = array();
+    $table['cols'] = array(
+    
+    array('label' => 'year', 'type' => 'string'),
+    array('label' => 'max', 'type' => 'number'),
+    array('label' => 'min', 'type' => 'number'),
+    array('label' => 'avg', 'type' => 'number'),
+    array('label' => 'median','type' => 'number'),
+
+
+);
+    $rows = array();
+    while($r = mysql_fetch_assoc($query)) {
+    $temp = array();
+    $temp[] = array('v' => $r['year']);
+    $temp[] = array('v' => $r['max']);
+    $temp[] = array('v' => $r['min']);
+    $temp[] = array('v' => $r['avg']);
+    $temp[] = array('v' => $r['median']);
+   
+   
+    
+    $rows[] = array('c' => $temp);
+}
+// populate the table with rows of data
+$table['rows'] = $rows;
+
+// encode the table as JSON
+$jsonTable = json_encode($table);
+
+// set up header; first two prevent IE from caching queries
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+// return the JSON data
+
+echo $jsonTable;
+
+?>
---
+++ yearly.php
@@ -0,0 +1,61 @@
+<html>
+  <head>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript" src="include/jsapi"></script>
+    <script type="text/javascript" src="include/jquery.min.js"></script>
+    <script type="text/javascript">
+
+    
+    
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function displayGraph(){
+    var station = document.getElementById('myStation').value;
+    var stationtwo = document.getElementById('myStationTwo').value;
+    
+
+    var content = document.getElementById("content");
+    document.getElementById("body").innerHTML = "";
+    document.getElementById("body").appendChild(content);
+      
+    function drawChart() {
+      var jsonData = $.ajax({
+          url: "getYearlyData.php?&station="+station+"&stationtwo="+stationtwo,
+          dataType: "json",
+          async: false
+          }).responseText;
+          
+      // Create our data table out of JSON data loaded from server.
+      var data = new google.visualization.DataTable(jsonData);
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+      chart.draw(data, {width: 1200, height: 550});
+    }
+    drawChart();
+  }
+
+    </script>
+  <body id="body">
+<div id="content">
+<form action ="" method ="POST">
+        <td>
+        Year From: <input type="text" name="station" id="myStation"> 
+        </td>
+        <td>
+        To: <input type="text" name="stationtwo" id="myStationTwo"> 
+        </td>
+        <td valign="top">
+        <input type="button" class="action" onclick="displayGraph();" value="Display Chart" />
+        </td>
+</tr>
+
+</form>
+<div id="chart_div"></div>
+</div>
+
+</body>
+</html>
